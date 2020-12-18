585d1846fe8b92548d4d23aa9bd44f51893bf2e7
Eric Garnett <egcodes@gmail.com>
2020-12-18 18:19:23 -0500

    implement functionality to sense scrolling up

--- travel-team-app/app/assets/scripts/modules/StickyHeader.js
+++ travel-team-app/app/assets/scripts/modules/StickyHeader.js
@@ -6,6 +6,7 @@ class StickyHeader {
     this.siteHeader = document.querySelector('.site-header')
     this.pageSections = document.querySelectorAll('.page-sections')
     this.browserHeight = window.innerHeight
+    this.previousScrollY = window.scrollY
     this.events()
   }
 
@@ -17,6 +18,8 @@ class StickyHeader {
   }
 
   runOnScroll() {
+    this.determineScrollDirection()
+
     if (window.scrollY > 60) {
       this.siteHeader.classList.add('site-header--dark')
     } else {
@@ -26,10 +29,19 @@ class StickyHeader {
     this.pageSections.forEach(el => this.calcSection(el))
   }
 
+  determineScrollDirection() {
+    if (window.scrollY > this.previousScrollY) {
+      this.scrollDirection = 'down'
+    } else {
+      this.scrollDirection = 'up'
+    }
+    this.previousScrollY = window.scrollY
+  }
+
   calcSection(el) {
     if (window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
       let scrollPercent = el.getBoundingClientRect().y / this.browserHeight * 100
-      if (scrollPercent < 18 && scrollPercent > -0.01) {
+      if (scrollPercent < 18 && scrollPercent > -0.01 && this.scrollDirection === 'down' || scrollPercent < 33 && this.scrollDirection === 'up') {
         let matchingLink = el.getAttribute('data-matching-link')
         document.querySelectorAll(`.primary-nav a:not(${matchingLink})`).forEach(link => link.classList.remove('is-current-link'))
         document.querySelector(matchingLink).classList.add('is-current-link')
